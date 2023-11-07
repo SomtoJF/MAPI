1b0024ee21aca569fc656977ac43a8e821b72d34
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-07 05:08:11 +0100

    define subsciber model schema

---
+++ models/subscribers.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const subscibersSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	subscribeToChannel: {
+		type: String,
+		required: true,
+	},
+	subscribeDate: {
+		type: Date,
+		required: true,
+		default: Date.now(),
+	},
+});
+
+module.exports = mongoose.model("Subscriber", subscibersSchema);
