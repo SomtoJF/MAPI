e41b018beb48699c9b91da5ad6b698c0d9b80682
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-07 05:03:13 +0100

    Initial commit

---
+++ routes/subscribers.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+router
+	.route("/")
+	.get((req, res) => {})
+	.post((req, res) => {});
+
+router
+	.route("/:id")
+	.get((req, res) => {})
+	.delete((req, res) => {})
+	.patch((req, res) => {});
+
+module.exports = router;
