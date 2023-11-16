1bad88f96234e0a6bf44f1f2e93b7aa243b7a026
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-16 20:56:09 +0100

    Write API to get and post a subscriber

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const Subscriber = require("../models/subscribers");
 
 router
 	.route("/")
-	.get((req, res) => {})
-	.post((req, res) => {});
+	.get(async (req, res) => {
+		try {
+			const subscribers = await Subscriber.find();
+			res.json(subscribers);
+		} catch (err) {
+			res.status(500).json({ message: err.message });
+		}
+	})
+	.post(async (req, res) => {
+		const requestBody = req.body;
+		const subscriber = new Subscriber({
+			name: requestBody.name,
+			subscribedToChannel: requestBody.subscribedToChannel,
+			subscribeDate: requestBody.subscribeDate,
+		});
+		try {
+			const newSubscriber = await subscriber.save();
+			res.status(201).json(newSubscriber);
+		} catch (err) {
+			res.status(400).json({ message: err.message });
+		}
+	});
 
 router
 	.route("/:id")
